ba944d54d9fbb92daa158db7d954752f049dbe46
justiandevs <30655756+justiandevs@users.noreply.github.com>
2017-11-14 10:05:49 +0100

    opdracht2geupdated
    Opdracht 1 geupdated, en opdracht 2.

--- opdracht1en2.py
+++ opdracht1en2.py
@@ -10,59 +10,45 @@ glazen = input("Hoeveel glazen bier heeft u gedronken: ")
 geslacht = input("Bent u een man of vrouw antwoord met m of v of man of vrouw: ")
 uur = input("Hoeveel uur geleden dronk u het eerste glas: ")
 
-#a * 10 uitrekenen. Variable Alcoholpro aanvast zetten.
-alcoholpro = int(glazen) * 10
-#(u - 0.5) uitrekenen. Variable uren aanvast zetten.
-uren = int(uur) - 0.5
-#(g * 0.002) uitrekenen. Variable kilo aanvast zetten.
-kilo = int(gewicht) * 0.002
+#nagaan of geslacht vrouw/man is, rekengedeelte koppelen.
+if geslacht == "man" or geslacht == "m":
+    rekengedeelte = 0.7
+elif geslacht == "vrouw" or geslacht == "v":
+    rekengedeelte = 0.5
+else: 
+    print("Dat is geen goede input.")
 
-#tweede deel van de totale formule (uren * kilo) uitrekenen.
-deeltwee = uren * kilo
+#rekenen
+som = ((int(glazen) * 10) / (int(gewicht) * rekengedeelte)) - (int(uur) - 0.5) * (int(gewicht) * 0.002)
 
-#deel van de code voor mannen, maat voor lichaamsvocht vastgezet op 0.7
-if geslacht == "m" or "man":
-    #andere deel van de formule uitrekenen (alcoholpro (hierboven) / gewicht * 0.7 (g * r in de formule.))
-    deeleenman = int(alcoholpro) / int(gewicht) * 0.7
-    #eindresultaat uitrekenen (deel een van de formule - deel twee)
-    resultaatzondermath = int(deeleenman) - int(deeltwee)
-    #variable resultaat gelijkzetten aan resultaatzondermath
-    resultaat = resultaatzondermath
-
-#deel van de code voor vrouwen, maat voor lichaamsvocht vastgezet op 0.5
-elif geslacht == "v" or "vrouw":
-    #andere deel van de formule uitrekenen (alcoholpro (hierboven) / gewicht * 0.5 (g * r in de formule.))
-    deeleenvrouw = int(alcoholpro) / int(gewicht) * 0.5
-    #eindresultaat uitrekenen (deel een van de formule - deel twee)
-    resultaatzondermath = int(deeleenvrouw) - int(deeltwee)
-    #variable resultaat gelijkzetten aan resultaatzondermath
-    resultaat = resultaatzondermath
-
-#toestanden binden aan het resultaat van promillage.
-if resultaat < 0.5:
+#toestanden binden aan het som van promillage.
+if som < 0.5:
     toestand = "opgewekt"
-
-elif resultaat >= 0.5 and resultaat < 1.5:
+elif som >= 0.5 and som < 1.5:
     toestand = "aangeschoten"
-
-elif resultaat >= 1.5 and resultaat < 3.0:
+elif som >= 1.5 and som < 3.0:
     toestand = "zat"
-
-elif resultaat >= 3.0 and resultaat < 4.0:
+elif som >= 3.0 and som < 4.0:
     toestand = "ladderzat"
-
-elif resultaat >= 4.0:
+elif som >= 4.0:
     toestand = "Laveloos, voor pampus"
 
-#rijtoestand binden aan het resultaat van promillage
-if resultaat < 0.5:
+#rijtoestand binden aan het som van promillage
+if som < 0.5:
     rijtoestand = "U mag rijden."
-
 else:
     rijtoestand = "Het is te hoog ga niet meer achter het stuur als dit te hoog is!"
 
 #alcoholpromillage weergeven + toestand.
-print("Uw alcoholpromillage is:", resultaat, "Uw toestand is:", toestand)
+print("Uw alcoholpromillage is:", round(som), "Uw toestand is:", toestand)
 #opdracht 2 deel = rijtoestand weergeven.
 print("Uw rijtoestand is:", rijtoestand)
+
+#opdracht2
+print("Zolang het alcoholpromillage onder de 0.5 is mag er nog worden gereden, uw toestand wordt hierboven gezegd en als u nog mag rijden wordt dat er aangegeven.")
+print("Je mag nog met 2 biertjes rijden, met 3 niet meer volgens de berekening.")
+print("Met twintig biertjes moet je langer dan 30 uur wachten.")
+
+
+    
 
